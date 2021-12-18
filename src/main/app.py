8b1cdb719b3b8d10fb17371344a34ba04abb4490
olwflynn <36010319+olwflynn@users.noreply.github.com>
2021-12-18 13:33:09 +0000

    refactored to add subscribe endpoint to app and centralised the functionality of starting everything in app.py

--- src/main/app.py
+++ src/main/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template, make_response
 import json, yaml, os
 from kafka import KafkaConsumer, KafkaProducer
+from .subscribe import connect_subscriptions
 
 def create_app():
 
@@ -9,19 +10,21 @@ def create_app():
     basedir = os.path.abspath(os.path.dirname(__file__))
 
     with open(os.path.join(basedir, 'contracts.yml'), 'r') as stream:
-        contracts = yaml.safe_load(stream)
+        contracts_yaml = yaml.safe_load(stream)
 
     producer = KafkaProducer(
         bootstrap_servers = app.config.get('KAFKA_SERVER'),
         api_version = (0, 11, 15)
     )
 
+    topic = app.config.get('TOPIC_NAME')
+
     @app.route("/")
     def home():
         return render_template("index.html",
                                title="Admin Panel",
                                header="This is where you can connect to contracts!",
-                               all_contracts=contracts)
+                               all_contracts=contracts_yaml)
 
     @app.route("/kafka/pushTransaction", methods=["POST"])
     def kafkaProducer():
@@ -29,7 +32,7 @@ def create_app():
         json_payload = json.dumps(req)
         json_payload = str.encode(json_payload)
         print("trying to send to kafka")
-        producer.send(app.config.get('TOPIC_NAME'), json_payload)
+        producer.send(topic, json_payload)
         producer.flush()
         print("Sent to kafka")
         return jsonify({"message": "You sent a cool message to kafka", "status": "pass"})
@@ -45,6 +48,10 @@ def create_app():
             200,
         )
 
+    @app.route("/api/v1/subscribe", methods=["GET"])
+    def subscribe():
+        return connect_subscriptions(producer, topic, contracts_yaml)
+
     return app
 
 
@@ -53,11 +60,10 @@ if __name__ == "__main__":
     app.run(debug=True, port = 5000)
 
 
-##TODO rm -r /tmp/zookeeper1 and /tmp/zookeeper once have taken kafka down
 ##TODO create docker image of the flask app for docker
-
 ##TODO figure out if we actually need endpoint to post events to. What else do we need endpoints e.g. choose contracts
-##TODO create Admin UI to add and store contracts and abis for which to subscribe to
+##TODO create Admin UI to add and store contracts and abis for which to subscribe to and configure which topics to write to
 ##TODO do something crypto specific value add with the events; enable to switch between blockchains by config and add event names
 ##TODO push kafka events to db or analytics
 ##TODO make the base template nicer and extend the others
+##TODO create cancel and update subscription endpoint
--- src/main/subscribe.py
+++ src/main/subscribe.py
@@ -1,49 +1,49 @@
 # import the following dependencies
-import json
-
+from kafka import KafkaConsumer, KafkaProducer
 import requests
 from web3 import Web3
 import asyncio
-import os, yaml
+import os, yaml, json
 
-# add your blockchain connection information
-infura_url = 'http://127.0.0.1:8545/'
-web3 = Web3(Web3.HTTPProvider(infura_url))
+def web3_setup(contracts_yaml_):
+    # add your blockchain connection information
+    infura_url = 'http://127.0.0.1:8545/'
+    web3 = Web3(Web3.HTTPProvider(infura_url))
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+    #TODO also need to add and pull out the topics from the config
+    # there is only one item = 'contracts' and doc = the dicts of contract addresses and abis
+    # create array of web3 contracts
+    web3_contracts = []
+    for item, doc in contracts_yaml_.items():
+        for address, abi_dict in doc.items():
+            abi = json.loads(abi_dict['abi'])
+            contract = web3.eth.contract(address=address, abi=abi)
+            web3_contracts.append(contract)
 
-with open(os.path.join(basedir, 'contracts.yml'), 'r') as stream:
-    contracts_yaml = yaml.safe_load(stream)
+    return web3_contracts
 
-
-# there is only one item = 'contracts' and doc = the dicts of contract addresses and abis
-# create array of web3 contracts
-web3_contracts = []
-for item, doc in contracts_yaml.items():
-    for address, abi_dict in doc.items():
-        abi = json.loads(abi_dict['abi'])
-        contract = web3.eth.contract(address=address, abi=abi)
-        web3_contracts.append(contract)
-
-print(web3_contracts)
 
 # define function to handle events and print to the console
-def handle_event(event):
+def handle_event(event, producer_, topic_):
     event_json = Web3.toJSON(event)
     print(event_json)
-    response = requests.post('http://localhost:5000/kafka/pushTransaction',json=event_json)
-    return response
+    # response = requests.post('http://localhost:5000/kafka/pushTransaction',json=event_json)
+    event_json = str.encode(event_json)
+    producer_.send(topic_, event_json)
+    producer_.flush()
+    print("Sent message to kafka")
+
 
 
 # asynchronous defined function to loop
 # this loop sets up an event filter and is looking for new entires for the "PairCreated" event
 # this loop runs on a poll interval
-async def log_loop(contracts, poll_interval):
+async def log_loop(contracts, poll_interval, producer_, topic_):
     filters = create_filters(contracts)
     while True:
         for event_filter in filters:
             for StorageEvent in event_filter.get_new_entries():
-                handle_event(StorageEvent)
+                handle_event(StorageEvent, producer_, topic_)
             await asyncio.sleep(poll_interval)
 
 def create_filters(contracts):
@@ -53,25 +53,31 @@ def create_filters(contracts):
         event_filter_array.append(event_filter)
     return event_filter_array
 
+def get_or_create_eventloop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return asyncio.get_event_loop()
+
 # when main is called
 # create a filter for the latest block and look for the "PairCreated" event for the uniswap factory contract
 # run an async loop
 # try to run the log_loop function above every 2 seconds
-def main():
-
-    #block_filter = web3.eth.filter('latest')
-    # tx_filter = web3.eth.filter('pending')
-    loop = asyncio.get_event_loop()
+def connect_subscriptions(producer, topic, contracts_yaml):
+    web3_contracts = web3_setup(contracts_yaml)
+    loop = get_or_create_eventloop()
+    print("starting the loop")
     try:
         loop.run_until_complete(
             asyncio.gather(
-                log_loop(web3_contracts, 2)))
-        # log_loop(block_filter, 2),
-        # log_loop(tx_filter, 2)))
+                log_loop(web3_contracts, 2, producer, topic)))
     finally:
         # close loop to free up system resources
         loop.close()
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     connect_subscriptions()
